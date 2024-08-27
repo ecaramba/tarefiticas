bc9642d04e2273b9d145361baad65fcf69169e69
Prof Edir <ecaramba@gmail.com>
2024-08-26 21:42:47 -0300

    carregnado tarefas

--- frontend/principal.js
+++ frontend/principal.js
@@ -2,8 +2,10 @@ $(document).ready(function(){
 
     $.getJSON("/tarefas.json", function(valores){
         
-        valores.forEach(function(item){
-            let nova = "<li>" 
+        console.log(valores)
+
+        valores.forEach(function(item, idx){
+            let nova = '<li codigo="'+idx+'" class="tarefa-item task-item-empty">' 
                 + "<b>"+ item.titulo +"</b>"
                 + "<p>"+ item.descricao +"</p>"
                 +"</li>";
@@ -13,4 +15,40 @@ $(document).ready(function(){
 
     });// fim do getJson
 
+    $("#bt-salvar").click(function(){
+
+        $("input, textarea").removeClass("is-invalid");
+
+        if( $("#titulo-tarefa").val() == "" )
+        {
+            $("#titulo-tarefa").addClass("is-invalid");
+        }
+
+        if ($("#descricao-tarefa").val() == "")
+        {
+            $("#descricao-tarefa").addClass("is-invalid");
+        }
+
+
+
+    }); //fim do click bt-salvar
+
+    $("body").on("click", ".tarefa-item", function(){
+        $("#modalTarefa").modal("show");
+        let codigo = $(this).attr("codigo");
+
+        carregarTarefa(codigo, function(dados){
+
+            console.log(dados);
+        });
+        
+    }); // fim do click
+
 });
+
+function carregarTarefa(id, callback)
+{
+    $.getJSON("/tarefas.json", function(valores){
+        callback(valores[id])
+    })
+}
